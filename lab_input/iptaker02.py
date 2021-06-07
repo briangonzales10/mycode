6fe6fd4569b9949fbc8facbd7d8132d4a0e65488
Brian Gonzales <brian.gonzales10@gmail.com>
2021-06-07 19:27:48 +0000

    a few example python scripts

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+user_input = input("Please enter an IPv4 IP Address:")
+print("You told me the IPv4 address is:", user_input)
+
+vendor_name = input("What is the vendor name?")
+print("Vendor name is", vendor_name)
+
